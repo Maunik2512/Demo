ec47280cefb90011f9370cd45eeceb281f6aea3c
Maunik2512 <maunik12@gmail.com>
2021-02-22 15:06:22 +0530

    Added start.c 1st time

---
+++ start.c
@@ -0,0 +1,18 @@
+/*
+* start.c − Illustration of multi filed modules
+*/
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* We're doing kernel work */
+/* Specifically, a module */
+int init_module(void)
+{
+	printk(KERN_INFO "Hello, world − this is the kernel speaking\n");
+	return 0;
+}
+
+void cleanup_module()
+{
+	printk(KERN_INFO "Short is the life of a kernel module\n");
+}
